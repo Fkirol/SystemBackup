f5cb6823e28cd15c2d863107de7e3b40d1f09909
Fkirol <lukushikirol@gmail.com>
2025-04-22 14:56:20 -0400

    Database_initalizer ok

--- database_inizializer.py
+++ database_inizializer.py
@@ -3,7 +3,7 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Backend.settings")
 django.setup()
 from django.contrib.auth import get_user_model
-User = get_user_model
+User = get_user_model()
 
 def create_superuser():
     if not User.objects.filter(is_superuser=True).exists():
