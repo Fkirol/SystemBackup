44f612c656504dcf988f96bb77f74eb0d70140ad
Fkirol <lukushikirol@gmail.com>
2025-04-22 21:08:20 -0400

    Superuser

--- database_inizializer.py
+++ database_inizializer.py
@@ -5,16 +5,10 @@ django.setup()
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
-def create_superuser():
-    if not User.objects.filter(is_superuser=True).exists():
-        superuser = User.objects.create_superuser(
-            username="admin",  # Reemplaza con el nombre de usuario deseado
-            email="admin@example.com",  # Reemplaza con el correo electrónico deseado
-            password="adminpassword",  # Reemplaza con la contraseña deseada (¡no uses esto en producción!)
-        )
-        print("Superusuario creado:", superuser.username)
-    else:
-        print("Superusuario ya existe.")
-
-if __name__ == "__main__":
-    create_superuser()
+try:
+    User.objects.get(username='admin')
+except:
+    User.objects.create_superuser(
+        username="admin",
+        password="admin1234",        
+    )
