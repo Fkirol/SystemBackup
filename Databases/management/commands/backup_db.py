5f09c2d9ab023e631b51d194fe837b4fb075b9a9
Fkirol <lukushikirol@gmail.com>
2025-04-29 15:03:50 -0400

    a ver que pasa

--- Databases/management/commands/backup_db.py
+++ Databases/management/commands/backup_db.py
@@ -12,10 +12,6 @@ from django.core.exceptions import ImproperlyConfigured
 from cryptography.fernet import Fernet
 from django.db import transaction
 from django.db.models import Q
-from concurrent.futures import ThreadPoolExecutor, as_completed
-import boto3
-from google.cloud import storage as gcs_storage
-
 
 # Configuración del logger
 logger = logging.getLogger(__name__)
@@ -27,62 +23,54 @@ logger.addHandler(handler)
 
 
 class Command(BaseCommand):
-    help = 'Crea backups de las bases de datos registradas en el modelo Database y los encripta usando OpenSSL, en paralelo'
+    help = 'Crea backups de las bases de datos registradas en el modelo Database y los encripta usando OpenSSL'
 
-    def handle(self, *args, **options):
-        # 1. Recuperamos solo los backups pendientes/expirados
-        backups = Backup.objects.filter(state__in=[0,1], date_init__lt=timezone.now())
-
-        # 2. Creamos un pool de hilos
-        max_workers = min(5, backups.count())  # ajusta grado de paralelismo
-        with ThreadPoolExecutor(max_workers=max_workers) as executor:
-            # 3. Para cada instancia de backup lanzamos submit() que disparará backup_database()
-            future_to_backup = {
-                executor.submit(self._run_backup_workflow, b): b
-                for b in backups
-            }
-
-            # 4. Opcionalmente, esperamos a que vayan terminando y registramos
-            for future in as_completed(future_to_backup):
-                b = future_to_backup[future]
-                try:
-                    future.result()
-                    logger.info(f"[{b.id_backup}] Backup terminado correctamente.")
-                except Exception as e:
-                    logger.error(f"[{b.id_backup}] Error en hilo de backup: {e}")
-
-    def _run_backup_workflow(self, backup_instance):
-        try:
-            frequency = backup_instance.id_database.id_frecuenly.time
-            new_date_init = backup_instance.date_init + frequency
+    #def handle(self, *args, **options):
+    #    for database_instance in Database.objects.all():
+    #        try:
+    #            self.crear_backup_expirado()
+    #            self.backup_database(database_instance)
+    #        except Exception as e:
+    #            logger.exception(f"Error inesperado al procesar la base de datos {database_instance.name}: {e}")
+    #            # Considera si quieres continuar con las otras bases de datos o no
                 
-            Backup.objects.create(id_database=backup_instance.id_database,
-                date_init=new_date_init,
-                date_finishing=timezone.now(),
-                location=backup_instance.location,
-                state=1)
-            db = backup_instance.id_database
-        
-            
-            backup_instance.state = 3 
-            backup_instance.save()
+    def handle(self):
+        backup_instances_actualizados = []  
 
-            # Ejecuta backup + encriptado
-            self.backup_database(db)
-            logger.info(f"Creando la Base De Datos...")
-            
+        for backup_instance in Backup.objects.filter(Q(state=1) | Q(state=0), date_init__lt=timezone.now()):  
+            try:
+                frequency = backup_instance.id_database.id_frecuenly.time
+                new_date_init = backup_instance.date_init + frequency
+                
+                Backup.objects.create(id_database=backup_instance.id_database,
+                    date_init=new_date_init,
+                    date_finishing=timezone.now(),
+                    location=backup_instance.location,
+                    state=1)
+                
+                backup_instance.state = 3
+                backup_instance.save()
+                
+                    
+                backup_instances_actualizados.append(backup_instance)
+                logger.info(f"Creando las Basese de Datos...")
 
-            # al terminar:
-            backup_instance.state = 2  # Successful
-            backup_instance.date_finishing = timezone.now()
-            backup_instance.save()
-        except Exception as e:
-            logger.exception(f"Error al crear un backup {backup_instance.id_backup}: {e}")
-            backup_instance.state = 0
-            backup_instance.save()
+                self.backup_database(backup_instance.id_database)
+                    
+                backup_instance.state = 2
+                backup_instance.date_finishing = timezone.now()
+                backup_instance.save()
+                    
+                
+                logger.info(f"Estado de la copia de seguridad{backup_instance.state}")
 
+            except Exception as e:
+                logger.exception(f"Error al crear un backup {backup_instance.id_backup}: {e}")
+                backup_instance.state = 0
+                backup_instance.save()
                 
 
+        return backup_instances_actualizados  
     def backup_database(self, database_instance):
         db_type = database_instance.id_type.name.lower()
         db_name = database_instance.name
@@ -100,12 +88,15 @@ class Command(BaseCommand):
             raise Exception(f"Error al obtener/desencriptar la contraseña: {e}")
 
         # 1.1 Validación del tipo de base de datos
-        valid_db_types = ['postgresql', 'mysql', 'sqlite', 'mongodb']
+        valid_db_types = ['postgresql', 'mysql', 'sqlite']
         if db_type not in valid_db_types:
             msg = f'Tipo de base de datos no soportado: {db_type}'
             logger.error(msg)
+           # self.crear_registro_backup(database_instance, 0, '', msg)  # Creamos el registro.
             return
 
+        # 2. Crear el registro de backup
+       # backup_record = self.crear_registro_backup(database_instance, 1)
 
         # 3. Construir el nombre del archivo de backup
         timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
@@ -175,47 +166,17 @@ class Command(BaseCommand):
                 db_path,
                 f'.backup "{backup_path}"'
             ]
-            
-        elif db_type == 'mongodb':
-            mongodump = shutil.which("mongodump")
-            if not mongodump:
-                raise CommandError("mongodump no encontrado en el PATH.")
-            dump_dir = backup_path.replace('.gz', '')  # Mongo crea carpetas
-            command = [
-                mongodump,
-                f'--username={db_user}',
-                f'--password={db_password}',
-                f'--db={db_name}',
-                f'--out={dump_dir}'
-            ]
-            if db_host:
-                command += [f'--host={db_host}']
-            if db_port:
-                command += [f'--port={db_port}']
-            env["MONGODB_URI"] = f"mongodb://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
-
-        else:
-            raise CommandError(f"Tipo de base de datos no soportado: {db_type}")
         return command, env
 
 
 
 
     #ENCRIPTACION
-    def execute_command(self, command, env, db_name, backup_path,db_password,**kwargs):
+    def execute_command(self, command, env, db_name, backup_path,db_password):
         try:
             result = subprocess.run(command, env=env, capture_output=True, text=True, check=True)
             logger.info(f'Backup de {db_name} creado en: {backup_path}')
             logger.debug(f'Salida del comando: {result.stdout}')
-            
-            db_type = kwargs.get('db_type', 'unknown')
-            
-            if db_type == 'mongodb':
-                import tarfile
-                tar_path = backup_path + ".tar.gz"
-                with tarfile.open(tar_path, "w:gz") as tar:
-                    tar.add(backup_path, arcname=os.path.basename(backup_path))
-                backup_path = tar_path  # usamos el tar.gz para cifrar
 
             # 6. Encriptar el backup utilizando OpenSSL
             encryption_password = db_password
@@ -232,9 +193,6 @@ class Command(BaseCommand):
             # Eliminar el archivo de backup sin encriptar
             os.remove(backup_path)
             logger.debug("Archivo de backup sin encriptar eliminado.")
-            
-            
-            self.store_backup(encrypted_backup_path)
 
 
         except subprocess.CalledProcessError as e:
@@ -248,51 +206,5 @@ class Command(BaseCommand):
             msg = f"Comando no encontrado: {e}"
             logger.error(msg)
            
-           
-    def store_backup(self, local_encrypted_path):
-    # Aquí accedemos a la location
 
-        # Encuentra el Backup relacionado al archivo actual
-        filename = os.path.basename(local_encrypted_path)
-        backup = Backup.objects.filter(location__isnull=False, state=1).order_by('-date_init').first()
-        if not backup:
-            logger.warning("No se encontró Backup en proceso para asociar la ubicación.")
-            return
-
-        destination = backup.location.strip()
-
-        if destination.startswith('s3://'):
-            # Subir a Amazon S3
-            import boto3
-            from urllib.parse import urlparse
-
-            parsed = urlparse(destination)
-            bucket = parsed.netloc
-            key_prefix = parsed.path.lstrip('/')
-            s3_key = os.path.join(key_prefix, filename)
-
-            s3 = boto3.client('s3')
-            s3.upload_file(local_encrypted_path, bucket, s3_key)
-            logger.info(f"Backup subido a Amazon S3 en: s3://{bucket}/{s3_key}")
-
-            # Borrar archivo local después de subir
-            os.remove(local_encrypted_path)
-
-        elif destination.startswith('gs://'):
-            # Subir a Google Cloud Storage
-            from google.cloud import storage
-            from urllib.parse import urlparse
-
-            parsed = urlparse(destination)
-            bucket = parsed.netloc
-            prefix = parsed.path.lstrip('/')
-            gcs_blob_name = os.path.join(prefix, filename)
-
-            storage_client = storage.Client()
-            bucket = storage_client.bucket(bucket)
-            blob = bucket.blob(gcs_blob_name)
-            blob.upload_from_filename(local_encrypted_path)
-            logger.info(f"Backup subido a Google Cloud Storage en: gs://{bucket.name}/{gcs_blob_name}")
-
-            # Borrar archivo local después de subir
-            os.remove(local_encrypted_path)
+    
